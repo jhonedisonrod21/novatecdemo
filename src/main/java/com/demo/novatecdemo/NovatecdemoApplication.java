2b584757ab4aa204b875ccb4dd886f470897448b
jhonedisonrod21 <jhon.rodriguez04@uptc.edu.co>
2022-09-19 19:16:28 -0500

    Comit inicial

---
+++ src/main/java/com/demo/novatecdemo/NovatecdemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.novatecdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovatecdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovatecdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/novatecdemo/NovatecdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.novatecdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovatecdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
